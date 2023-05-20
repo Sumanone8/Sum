326888c2c8dd55fbff7311c3098076b05ec78f7b
Sumanone8 <sumanjhabhl@gmail.com>
2023-05-20 09:23:33 +0530

    Add:Sum og digits

---
+++ Sum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SumOfDigits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            Console.WriteLine("---Sum of Digits---");
+            Console.WriteLine("Enter the number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            while (num > 0)
+            {
+                int digit = num % 10;
+                sum += digit;
+                num = num / 10;
+            }
+            Console.WriteLine("Sum of digit is:" + sum);
+         }
+    }
+}
+
+
